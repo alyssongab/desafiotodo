b842732f55a4c3620b8b3f07fd21d7465007c891
Alysson Gabriel <alysson.gabriel61@gmail.com>
2025-04-03 17:22:15 -0400

    style: estiliza tabela de tarefas

--- frontend/src/components/Tabela.jsx
+++ frontend/src/components/Tabela.jsx
@@ -1,37 +1,35 @@
-const Tabela = () => {
+import { useState } from "react";
+import tarefas from '../data.js'
+import { Trash2 } from 'lucide-react';
+import { Pencil } from 'lucide-react';
+import { ChevronDown } from 'lucide-react';
 
-    const tarefas = [
-        {
-            id: 1,
-            titulo: "Janela",
-            descricao: "Fechar a janela antes de sair de casa",
-            status: "Pendente",
-            data: "2025-4-3"
-        },
-
-        {
-            id: 2,
-            titulo: "Comida",
-            descricao: "Guardar comida na geladeira antes de dormir",
-            status: "Concluída",
-            data: "2025-4-3"
-        }
-    ];
+const Tabela = ({titulo, descricao, status, dataCriacao}) => {
 
     const listaTarefas = tarefas.map(tarefa => {
     
         return(
-            <div id={tarefa.id} className="flex gap-2 pb-2">
-                <input type="checkbox" />
-                <p>{tarefa.descricao}</p>
+            <div id={tarefa.id} className="flex items-center justify-between">
+                <div className="flex">
+                    <input type="checkbox" className="w-5"/>
+                    <p className="text-xl p-3 flex items-center">{tarefa.titulo}<ChevronDown/></p>
+                </div>
+
+                <div className="flex gap-3 pr-3">
+                    <Trash2 className="cursor-pointer hover:scale-105"/>
+                    <Pencil className="cursor-pointer hover:scale-105"/>
+                </div>
+
             </div>
         );
     });
 
+    const [useText, setUseText] = useState(false);
+
     return(
         <>
-            <div className="bg-slate-200 w-[60%] rounded-sm">
-                <div className="flex flex-col gap-3 p-3">
+            <div className="bg-slate-200 w-[95%] sm:w-[80%] md:w-[60%] lg:w-[30%] rounded-sm">
+                <div className="flex flex-col gap-1 pl-2">
                     {listaTarefas}  
                 </div>
             </div>
---
+++ frontend/src/data.js
@@ -0,0 +1,27 @@
+const tarefas = [
+    {
+        id: 1,
+        titulo: "Janela",
+        descricao: "Fechar a janela antes de sair de casa",
+        status: "Pendente",
+        data: "2025-4-3"
+    },
+
+    {
+        id: 2,
+        titulo: "Comida",
+        descricao: "Guardar comida na geladeira antes de dormir",
+        status: "Concluída",
+        data: "2025-4-3"
+    },
+    
+    {
+        id: 3,
+        titulo: "Express",
+        descricao: "Estudar express framework",
+        status: "Concluída",
+        data: "2025-4-3"
+    }
+];
+
+export default tarefas;
